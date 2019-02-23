43828d3e5f5a848ad910d4a3422676a40ae94239
Teddy <teddychen1251@gmail.com>
2019-02-23 15:25:15 -0600

    Remove express from devDependenciesasdfasdfad

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.user(cors())
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
