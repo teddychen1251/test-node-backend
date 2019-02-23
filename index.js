bd9b589d8b05354b4098b6b87696dde2309ed2d3
Teddy <teddychen1251@gmail.com>
2019-02-23 15:17:46 -0600

    some code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+
+app.user(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
